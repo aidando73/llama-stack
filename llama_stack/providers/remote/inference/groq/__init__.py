0bc2cd67e9890268a209d1c20d870cb285520a23
Aidan Do <aidando73@gmail.com>
2024-12-12 10:48:31 +1100

    .

--- llama_stack/providers/remote/inference/groq/__init__.py
+++ llama_stack/providers/remote/inference/groq/__init__.py
@@ -8,6 +8,7 @@ async def get_adapter_impl(config: GroqConfig, _deps) -> Inference:
     # # import dynamically so `llama stack build` does not fail due to missing dependencies
     # from .groq import GroqInferenceAdapter
 
+    print("hello world")
     if not isinstance(config, GroqConfig):
         raise RuntimeError(f"Unexpected config type: {type(config)}")
 
--- llama_stack/providers/tests/inference/groq/test_init.py
+++ llama_stack/providers/tests/inference/groq/test_init.py
@@ -4,8 +4,10 @@ import pytest
 from llama_stack.providers.remote.inference.ollama import OllamaImplConfig
 
 class TestGroqInit:
-    def test_raises_runtime_error_if_config_is_not_groq_config(self):
+    @pytest.mark.asyncio
+    async def test_raises_runtime_error_if_config_is_not_groq_config(self):
         from llama_stack.providers.remote.inference.groq import get_adapter_impl
         config = OllamaImplConfig(model="llama3.1-8b-8192")
+
         with pytest.raises(RuntimeError):
-            get_adapter_impl(config, None)
+            await get_adapter_impl(config, None)
